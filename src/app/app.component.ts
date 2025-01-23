18177ec9f687e6354ae83b2b81b8f7bd4034f52e
Vrushi-Pansare <vrushalicshare@gmail.com>
2025-01-23 16:07:08 +0530

    Two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 
+import { CommonModule, NgClass, NgFor} from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports : [],
+  imports : [ CommonModule, FormsModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title: string ;
-  logo: string = '../assets/img/logo.png' ;
+   title: string ;
+  // logo: string = '../assets/img/logo.png' ;
   name: string = 'Todd';
+  handleClick(){
+    this.name = 'Motto' ;
+  }
+  handleChange(value: string){
+    this.name = value;
+  }
+
 
   // isHappy: boolean = false;
   // numberOne: number = 1;
